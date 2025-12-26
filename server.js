f58910d78c6c496eaf4eec2e74f52dd00b3990af
akbarjonovizzatillo9-tech <akbarjonovizzatillo9@gmail.com>
2025-12-25 20:43:49 -0800

    feat: develop author ejs page

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
-
+console.log("Web Serverni boshlash");
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if(err) {
+    console.log("ERROR:", err);
+  } else {
+    use = JSON.parse(data)
+  }
+});
 
 // 1: Kirish code
 app.use(express.static("public"));
@@ -20,8 +31,12 @@ app.post("/reja", (req, res) => {
   res.json({test: "success"});
 });
 
+app.get('/author', (req, res) => {
+  res.render("author", {user: user});
+});
+
 app.get("/", function (req, res) {
-  res.render("harid");
+  res.render("author", );
 });
 
 const server = http.createServer(app);
